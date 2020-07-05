207326d3bccd5645eeb2cbc080ca5f04df1597e5
Leonidas Esteban <leonidasesteban@gmail.com>
2020-07-04 21:42:36 -0500

    Updates

---
+++ precache-manifest.528925b71645e87848b4c3a70635009c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f201e3e2c950d4135d7ce695871bb781",
+    "url": "/rick-and-morty-react/index.html"
+  },
+  {
+    "revision": "676161baae1436a2b15b",
+    "url": "/rick-and-morty-react/static/css/main.16545c20.chunk.css"
+  },
+  {
+    "revision": "9ee9f343c0dae5a08dcd",
+    "url": "/rick-and-morty-react/static/js/2.b8302b31.chunk.js"
+  },
+  {
+    "revision": "a9dd647aec81fffade4fa7e6d693dad7",
+    "url": "/rick-and-morty-react/static/js/2.b8302b31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "676161baae1436a2b15b",
+    "url": "/rick-and-morty-react/static/js/main.d1824bc0.chunk.js"
+  },
+  {
+    "revision": "12e0f2fa3add8673b4be",
+    "url": "/rick-and-morty-react/static/js/runtime-main.ef7edaab.js"
+  }
+]);
